be7ffae4ecdd16b298e45b50dd8b093a8474d739
NatalieShif <natalia.shif@gmail.com>
2024-05-12 12:35:21 +0300

    random colors

--- js/random.js
+++ js/random.js
@@ -7,6 +7,15 @@ const out0To10 = document.querySelector('.from0To10 .out');
 const btn1To5 = document.querySelector('.from1To5 button');
 const out1To5 = document.querySelector('.from1To5 .out');
 
+const btnColors = document.querySelector('.colors button');
+const outColors = document.querySelector('.colors .out');
+
+// from 0 till some number
+const from0To = (tillNum) => {
+
+    return Math.floor(Math.random() * (tillNum+1));
+
+}
 
 /************************ LISTENERS ************************/
 
@@ -25,5 +34,14 @@ btn0To10.addEventListener('click', () => {
 btn1To5.addEventListener('click', () => {
 
     out1To5.innerText = Math.floor(Math.random() * 5) + 1;
+
+})
+
+btnColors.addEventListener('click',() => {
+
+    let colorStr = `rgb(${from0To(255)},${from0To(255)},${from0To(255)})`;
+    outColors.innerText = colorStr;
+    outColors.style.background = colorStr;
+
 
 })
